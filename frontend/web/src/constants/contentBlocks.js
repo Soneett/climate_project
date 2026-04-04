e256862a698bcbb66e27f19684df692fd6d490f4
Кукочкина София Александровна <sakukochkina@edu.hse.ru>
2026-04-04 13:57:50 +0300

    new roads

--- frontend/web/src/constants/contentBlocks.js
+++ frontend/web/src/constants/contentBlocks.js
@@ -1071,6 +1071,7 @@ export const CONTENT_BLOCKS = {
     {
       id: "s6-3",
       title: "Протяженность железных дорог",
+      indicators: ["Протяженность железных дорог"],
       chartType: "line",
       chartData: {
         labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
@@ -1082,6 +1083,7 @@ export const CONTENT_BLOCKS = {
     {
       id: "s6-4",
       title: "Протяженность водных путей",
+      indicators: ["Протяженность водных путей"],
       chartType: "line",
       chartData: {
         labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
